189cccf497a43824a4f7171525ad783483e8409a
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-01-22 11:38:14 +0900

    [#28] test vite 세팅 적용

--- packages/test/vite.config.mjs
+++ packages/test/vite.config.mjs
@@ -1,11 +1,12 @@
 import {createViteConfig} from '@naverpay/pite'
 
-export const testGlobArray = ['./src/**/*.ts', '!./src/**/*.bench.ts', '!./src/**/*.test.ts']
+export const testGlobArray = ['./src/**/*.ts', './src/**/*.tsx', '!./src/**/*.bench.ts', '!./src/**/*.test.ts']
 
 // @ts-check
 export default createViteConfig({
     formats: ['es', 'cjs'],
     outDir: ['dist/esm', 'dist/cjs'],
+    cssFileName: 'mergedStyle.css',
     cwd: '.',
     entry: testGlobArray,
     allowedPolyfills: ['es.array.find-last'],
