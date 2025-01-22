e9e5d06c934ef875c0e37cea4a2a9a01d1641224
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-01-22 15:54:06 +0900

    [#28] cssFileName도 옵션 적용

--- packages/pite/src/index.ts
+++ packages/pite/src/index.ts
@@ -56,6 +56,7 @@ export function createViteConfig({
     const build: BuildOptions = {
         target: browserslistToEsbuild(browserslist),
         lib: {
+            cssFileName: cssFileName.replace('.css', ''),
             formats,
             entry: getViteEntry(entry),
             ...inputLib,
