17bedca81fb5e972211552551e5c1065b96753aa
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-03-04 14:14:56 +0900

    [#61] 기본 external에 builtins 추가

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -1,14 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 
+import builtins from 'builtin-modules'
+
 export function getExternalDependencies(cwd: string) {
     const packageJSONPath = path.join(cwd, 'package.json')
     const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'))
 
-    let deps: string[] = []
+    let deps: string[] = [...builtins]
 
     if ('dependencies' in packageJSON && typeof packageJSON.dependencies === 'object') {
-        deps = [...Object.keys(packageJSON.dependencies)]
+        deps = [...deps, ...Object.keys(packageJSON.dependencies)]
     }
 
     if ('peerDependencies' in packageJSON && typeof packageJSON.peerDependencies === 'object') {
